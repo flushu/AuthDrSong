0949fdf9bd08d616d6790e147e2925b37c7667dc
fffflushu <ben@drsongvision.com>
2018-06-04 18:00:39 -0700

    updated

--- src/components/templates/superbill/Header.js
+++ src/components/templates/superbill/Header.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import AutosizeInput from 'react-input-autosize';
+import React from "react";
+import AutosizeInput from "react-input-autosize";
 
-class Header extends React.Component{
-  constructor(props){
+class Header extends React.Component {
+  constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
       <div id="header">
         <Row1 data={!!this.props.data ? this.props.data : undefined} />
         <Row2 />
       </div>
-    )
+    );
   }
-};
+}
 
-const Row1 = (props) => (
+const Row1 = props => (
   <div className="header_row1">
-    <Col1 data={!!props.data ? props.data : undefined}/>
-    <Col2 data={!!props.data ? props.data : undefined}/>
-    <Col3 data={!!props.data ? props.data : undefined}/>
+    <Col1 data={!!props.data ? props.data : undefined} />
+    <Col2 data={!!props.data ? props.data : undefined} />
+    <Col3 data={!!props.data ? props.data : undefined} />
   </div>
 );
 
-class Col1 extends React.Component{
-  constructor(props){
+class Col1 extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       value_chartup: "",
@@ -32,11 +32,19 @@ class Col1 extends React.Component{
       value_arrival: "",
       value_prep: "",
       provider: !!props.data ? props.data.provider : "",
-      location: !!props.data ? (props.data.date.includes("Mon") || props.data.date.includes("Tue") || props.data.date.includes("Thu") ? "Los Alamitos" : "Pasadena") : ""
+      // location: !!props.data ? "Pasadena" : "",
+      location: "Pasadena"
+      // !!props.data
+      //   ? props.data.date.includes("Mon") ||
+      //     props.data.date.includes("Tue") ||
+      //     props.data.date.includes("Thu")
+      //     ? "Los Alamitos"
+      //     : "Pasadena"
+      //   : ""
       // location: !!props.data ? "Pasadena" : ""
     };
-  }  
-  render(){
+  }
+  render() {
     return (
       <div className="header_col1">
         <div className="group">
@@ -83,12 +91,36 @@ class Col1 extends React.Component{
           </div>
           <div className="header_col1_row2">
             <div>
-              <label><input type="checkbox" checked={this.state.location.includes("Los Alamitos")} /> Los Alamitos</label>
-              <label><input type="checkbox" checked={this.state.location.includes("Pasadena")} /> Pasadena</label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={this.state.location.includes("Los Alamitos")}
+                />{" "}
+                Los Alamitos
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={this.state.location.includes("Pasadena")}
+                />{" "}
+                Pasadena
+              </label>
             </div>
             <div>
-              <label><input type="checkbox" checked={this.state.provider.includes("Alice Song")} /> Alice Song, M.D.</label>
-              <label><input type="checkbox" checked={this.state.provider.includes("Julia Song")} /> Julia Song, M.D.</label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={this.state.provider.includes("Alice Song")}
+                />{" "}
+                Alice Song, M.D.
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={this.state.provider.includes("Julia Song")}
+                />{" "}
+                Julia Song, M.D.
+              </label>
             </div>
           </div>
         </div>
@@ -97,27 +129,38 @@ class Col1 extends React.Component{
   }
 }
 
-class Col2 extends React.Component{
-  constructor(props){
+class Col2 extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       type: !!props.data ? props.data.type : "",
       note: !!props.data ? props.data.note : ""
-    }
+    };
   }
-  render(){
+  render() {
     return (
       <div className="header_col2">
         <div>
           <input type="checkbox" checked={this.state.type.includes("NP")} /> NP
-          <input type="checkbox" checked={this.state.type.includes("F/U")} /> F/U
-          <input type="checkbox" checked={this.state.type.includes("P/O")} /> P/O
+          <input
+            type="checkbox"
+            checked={this.state.type.includes("F/U")}
+          />{" "}
+          F/U
+          <input
+            type="checkbox"
+            checked={this.state.type.includes("P/O")}
+          />{" "}
+          P/O
           <input type="checkbox" checked={this.state.type.includes("SX")} /> SX
         </div>
         <div>
-          <textarea cols={60} rows={3} value={this.state.note} onChange={(e)=>{
-
-          }}/>
+          <textarea
+            cols={60}
+            rows={3}
+            value={this.state.note}
+            onChange={e => {}}
+          />
         </div>
       </div>
     );
@@ -130,39 +173,67 @@ class Col3 extends React.Component {
     this.state = {
       name: !!props.data ? props.data.name : "",
       dob: !!props.data ? props.data.dob : "",
-      apt_time: !!props.data ? props.data.date.substr(5) + " " + props.data.time : "",
+      apt_time: !!props.data
+        ? props.data.date.substr(5) + " " + props.data.time
+        : "",
       phone: !!props.data ? props.data.phone : ""
-    }
+    };
   }
   render() {
     return (
       <div className="header_col3">
         <div className="group">
-          <div>Name: <AutosizeInput  value={this.state.name} onChange={(e)=>{
-            this.setState({name: e.target.value})
-          }}/></div>
-          <div>Phone: <input id="phone_input" type='text' value={this.state.phone}
-            onChange={(e)=>{
-              const phone = e.target.value;
-              this.setState({
-                phone
-              });
-            }}
-          /></div>
-          <div>DOB: <input id="dob_input" type="text" value={this.state.dob} 
-            onChange={(e)=>{
-              const dob = e.target.value;
-              this.setState({
-                dob
-              });
-            }}/></div>
-          <div>Apt Time: <input id="apt_time_input" type="text" value={this.state.apt_time}
-            onChange={(e)=>{
-              const apt_time = e.target.value;
-              this.setState({
-                apt_time
-              });
-            }}/></div>
+          <div>
+            Name:{" "}
+            <AutosizeInput
+              value={this.state.name}
+              onChange={e => {
+                this.setState({ name: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            Phone:{" "}
+            <input
+              id="phone_input"
+              type="text"
+              value={this.state.phone}
+              onChange={e => {
+                const phone = e.target.value;
+                this.setState({
+                  phone
+                });
+              }}
+            />
+          </div>
+          <div>
+            DOB:{" "}
+            <input
+              id="dob_input"
+              type="text"
+              value={this.state.dob}
+              onChange={e => {
+                const dob = e.target.value;
+                this.setState({
+                  dob
+                });
+              }}
+            />
+          </div>
+          <div>
+            Apt Time:{" "}
+            <input
+              id="apt_time_input"
+              type="text"
+              value={this.state.apt_time}
+              onChange={e => {
+                const apt_time = e.target.value;
+                this.setState({
+                  apt_time
+                });
+              }}
+            />
+          </div>
         </div>
       </div>
     );
@@ -171,7 +242,9 @@ class Col3 extends React.Component {
 
 const Row2 = () => (
   <div className="header_row2">
-    Legends: // Eye(E): 1 OD, 2 OS, 3 OU // Lid(L): 1 RUL, 2 RLL, 4 LUL, 5 LLL // X: space holder // Y Severity: 1 mid, 2 moderate, 3 severe // Z encouter: A initial, D subsequent, S sequelac
+    Legends: // Eye(E): 1 OD, 2 OS, 3 OU // Lid(L): 1 RUL, 2 RLL, 4 LUL, 5 LLL
+    // X: space holder // Y Severity: 1 mid, 2 moderate, 3 severe // Z encouter:
+    A initial, D subsequent, S sequelac
   </div>
 );
 
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -9,15 +9,15 @@ const defaultState = {
 
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case 'SORT_BY_NAME':
-      return { ...state, sortBy: 'name' };
-    case 'SORT_BY_TIME':
-      return { ...state, sortBy: 'time' };
-    case 'SET_TEXT':
+    case "SORT_BY_NAME":
+      return { ...state, sortBy: "name" };
+    case "SORT_BY_TIME":
+      return { ...state, sortBy: "time" };
+    case "SET_TEXT":
       return { ...state, text: action.text };
-    case 'SET_DATE':
+    case "SET_DATE":
       return { ...state, date: action.date };
-    case 'HIDE_FINISH':
+    case "HIDE_FINISH":
       return { ...state, hideFinish: action.hideFinish };
     default:
       return state;
